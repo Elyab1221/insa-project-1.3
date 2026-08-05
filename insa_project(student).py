c4af9980bb0666b4d35a24b309e8da62c44f8489
Elyab <wondassaelyab@gmail.com>
2026-08-05 17:24:41 +0300

    added a value error checker

--- insa_project(student).py
+++ insa_project(student).py
@@ -1,18 +1,46 @@
 k = input("enter your name: ")
 
-l = float(input("enter your marks in sub1: "))
+l=-1
+
+try:
+   l = float(input("enter your marks in sub1: "))
+except ValueError:
+   print("please enter a valid number")
+
 while l > 100 or l < 0:
-    l = float(input("please enter your actual marks in sub1: "))
+    try:
+        l = float(input("please enter your actual marks in sub1: "))
+    except ValueError:
+        print("please enter a valid number")
+m=-1
+try:
+    m = float(input("enter your marks in sub2: "))
+except ValueError:
+    print("please enter a valid number")
 
-m = float(input("enter your marks in sub2: "))
 while m > 100 or m < 0:
-    m = float(input("please enter your actual marks in sub2: "))
+    try:
+        m = float(input("please enter your actual marks in sub2: "))
+    except ValueError:
+        print("please enter a valid number")
+n=-1
+try:
+    n = float(input("enter your marks in sub3: "))
+except ValueError:
+    print("please enter a valid number")
 
-n = float(input("enter your marks in sub3: "))
 while n > 100 or n < 0:
-    n = float(input("please enter your actual marks in sub3: "))
+    try:
+        n = float(input("please enter your actual marks in sub3: "))
+    except ValueError:
+        print("please enter a valid number")
 average = (l + m + n) / 3
-
+print("                  ")
+print("     report-card "      )
+print("                  ")
+print( "========================================== ")
+print("your name is: ", k)
+print("your average marks is: ", average)
 if average >= 80 and average <= 100:
     print("your grade is A")
 elif average >= 70 and average <= 79:
@@ -23,9 +51,6 @@ elif average >= 50 and average <= 59:
     print("your grade is D")
 else:
     print("your grade is F")
-
-print("your name is: ", k)
-print("your average marks is: ", average)
 
 if average >= 50:
     print("you have passed")
