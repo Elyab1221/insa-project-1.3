5481c93569853c754f747f812e19a4be41cce7b5
Elyab <wondassaelyab@gmail.com>
2026-08-06 10:48:10 +0300

    added the grade analyzer

--- task_4-login.py
+++ task_4-login.py
@@ -5,6 +5,66 @@ attempt=0
 while attempt<=5:
     if username=="admin" and password=="admin123":
         print("access granted")
+
+        # k = input("enter your name: ")
+
+        l=-1
+
+        try:
+            l = float(input("enter your marks in sub1: "))
+        except ValueError:
+            print("please enter a valid number")
+
+        while l > 100 or l < 0:
+            try:
+                l = float(input("please enter your actual marks in sub1: "))
+            except ValueError:
+                print("please enter a valid number")
+        m=-1
+        try:
+            m = float(input("enter your marks in sub2: "))
+        except ValueError:
+            print("please enter a valid number")
+
+        while m > 100 or m < 0:
+            try:
+                m = float(input("please enter your actual marks in sub2: "))
+            except ValueError:
+                print("please enter a valid number")
+        n=-1
+        try:
+            n = float(input("enter your marks in sub3: "))
+        except ValueError:
+            print("please enter a valid number")
+
+        while n > 100 or n < 0:
+            try:
+                n = float(input("please enter your actual marks in sub3: "))
+            except ValueError:
+                print("please enter a valid number")
+        average = (l + m + n) / 3
+        print("                  ")
+        print("     Report-Card "      )
+        print("                  ")
+        print( "========================================== ")
+        print("your name is: ", username)
+        print("your average marks is: ", average)
+        if average >= 80 and average <= 100:
+            print("your grade is A")
+        elif average >= 70 and average <= 79:
+            print("your grade is B")
+        elif average >= 60 and average <= 69:
+            print("your grade is C")
+        elif average >= 50 and average <= 59:
+            print("your grade is D")
+        else:
+            print("your grade is F")
+
+        if average >= 50:
+            print("you have passed")
+        else:
+            print("you have failed")
+
         break
     else:
         attempt+=1
